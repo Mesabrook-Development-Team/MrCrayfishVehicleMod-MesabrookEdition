2ca83d5867d27f9e6776e167d8b5bf1c0e8ccd94
Christopher <supersoftware1234@gmail.com>
2024-06-20 01:02:51 -0400

    Converted all recipes to use Steel.

--- src/main/java/com/mrcrayfish/vehicle/Reference.java
+++ src/main/java/com/mrcrayfish/vehicle/Reference.java
@@ -11,7 +11,8 @@ public class Reference
     public static final String MOD_VERSION = "0.44.1-0.0.3";
     public static final String MOD_COMPATIBILITY = "[1.12.2]";
     public static final String MOD_DEPENDS = "required-after:immersiveengineering;required-after:immersivepetroleum;required-after:obfuscate@[0.2.5,);after:cfm@[4.1.5,);after:controllable@[0.8.0,);required-after:forge@[14.23.4.2705,)";
-
+    public static final String LOG_PREFIX = "[" + MOD_NAME + "] ";
+    
     public static final String PROXY_CLIENT = "com.mrcrayfish.vehicle.proxy.ClientProxy";
     public static final String PROXY_SERVER = "com.mrcrayfish.vehicle.proxy.ServerProxy";
 }
